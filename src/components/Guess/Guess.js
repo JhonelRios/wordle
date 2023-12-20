f43430e12ba7260564404638fbaddaa9ea3a6415
jhonelrios <riosjaimes.j@gmail.com>
2023-12-19 23:27:52 -0500

    feat: game logic

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-function Guess({ guess }) {
+function Guess({ guess, answer }) {
+  const lettersStatus = checkGuess(guess, answer);
+
   return (
     <p className="guess">
-      {range(5).map(idx => (
-        <span className="cell" key={idx}>
-          {guess ? guess[idx] : ''}
-        </span>
-      ))}
+      {range(5).map(idx => {
+        const letter = guess ? guess[idx] : '';
+        const cellStatus = guess
+          ? lettersStatus.find(status => status.letter === letter)?.status
+          : '';
+
+        return (
+          <span className={`cell ${cellStatus}`} key={idx}>
+            {letter}
+          </span>
+        );
+      })}
     </p>
   );
 }
--- src/components/Guesses/Guesses.js
+++ src/components/Guesses/Guesses.js
@@ -3,11 +3,11 @@ import { range } from '../../utils';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import Guess from '../Guess/Guess';
 
-function Guesses({ guesses }) {
+function Guesses({ guesses, answer }) {
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map(idx => (
-        <Guess key={idx} guess={guesses[idx]} />
+        <Guess key={idx} guess={guesses[idx]} answer={answer} />
       ))}
     </div>
   );
